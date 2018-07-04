238fa08b4563c8357ff3885ddeeaec68c92bbdc4
tom statter <dev@autotelik.co.uk>
2018-07-04 21:15:49 +0200

    add paths to config.assets

--- app/helpers/datashift_audio/application_helper.rb
+++ app/helpers/datashift_audio/application_helper.rb
@@ -2,7 +2,7 @@ module DatashiftAudio
   module ApplicationHelper
 
     def datashift_audio_player_tag
-      "<h1>datahsift_audio_plauer_tag</h1>"
+      "<h1>datahsift_audio_player_tag</h1>"
     end
 
   end
--- lib/datashift_audio/engine.rb
+++ lib/datashift_audio/engine.rb
@@ -1,5 +1,13 @@
 module DatashiftAudio
   class Engine < ::Rails::Engine
     isolate_namespace DatashiftAudio
+
+    # Add a load path for this specific Engine
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+
+    config.assets.paths << root.join("app", "assets", "javascripts", "datashift_audio")
+    config.assets.paths << root.join("vendor", "assets", "javascripts")
+
+    config.assets.precompile += %w[*.png]
   end
 end
